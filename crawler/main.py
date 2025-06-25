245d1bfb376190db3effc69b79cdb34b393f08d5
MohammadYehya <mohammad7446@gmail.com>
2025-06-25 12:11:14 +0500

    Update main.py

--- crawler/main.py
+++ crawler/main.py
@@ -40,47 +40,40 @@ cursor = None
 
 # Crawl loop
 while count < limit:
-    qstr = f"stars:<{minstars}"
-    while True:
-        try:
-            print(f"Min Stars: {tempminstars}")
-            result = client.run_query(query, {"cursor": cursor, "queryStr": qstr})
-            break
-        except:
-          time.sleep(0.75)
-    if 'data' not in result or 'search' not in result['data']:
-        break
-    repos = result['data']['search']['nodes']
-    # for repo in repos:
-    #     cur.execute('''
-    #         INSERT INTO repositories (repo_id, name, stars)
-    #         VALUES (%s, %s, %s)
-    #         ON CONFLICT (repo_id) DO UPDATE
-    #         SET stars = EXCLUDED.stars, last_updated = CURRENT_TIMESTAMP;
-    #     ''', (repo['id'], repo['nameWithOwner'], repo['stargazerCount']))
-    #     count += 1
-    #     if count >= limit:
-    #         break
-    data = [(repo['id'], repo['nameWithOwner'], repo['stargazerCount']) for repo in repos]
-    if len(data) + count > limit:
-      data = data[:(limit-len(data)-count-1)]
-    cur.executemany('''
-              INSERT INTO repositories (repo_id, name, stars)
-              VALUES (%s, %s, %s)
-              ON CONFLICT (repo_id) DO UPDATE
-              SET stars = EXCLUDED.stars, last_updated = CURRENT_TIMESTAMP;
-          ''', data)
-    count += len(data)
-    if count >= limit:
+  qstr = f"stars:<{minstars}"
+  while True:
+    try:
+      qtime = time.time()
+      result = client.run_query(query, {"cursor": cursor, "queryStr": qstr})
+      print(f"Queried in {round(time.time() - qtime, 2)}s")
       break
-    if not result['data']['search']['pageInfo']['hasNextPage']:
-      minstars = tempminstars
-      cursor = None
-    else:
-      tempminstars = repos[-1]['stargazerCount']
-      cursor = result['data']['search']['pageInfo']['endCursor']
-        
-    time.sleep(0.75)
+    except:
+      time.sleep(0.75)
+  print(f"Min Stars: {tempminstars}")
+  if 'data' not in result or 'search' not in result['data']:
+    break
+  repos = result['data']['search']['nodes']
+  data = [(repo['id'], repo['nameWithOwner'], repo['stargazerCount']) for repo in repos]
+  if len(data) + count > limit:
+    data = data[:(limit-len(data)-count-1)]
+  itime = time.time()
+  cur.executemany('''
+            INSERT INTO repositories (repo_id, name, stars)
+            VALUES (%s, %s, %s)
+            ON CONFLICT (repo_id) DO UPDATE
+            SET stars = EXCLUDED.stars;
+        ''', data)
+  print(f"Inserted in {round(time.time()-itime, 2)}s")
+  count += len(data)
+  if count >= limit:
+    break
+  if not result['data']['search']['pageInfo']['hasNextPage']:
+    minstars = tempminstars
+    cursor = None
+  else:
+    tempminstars = repos[-1]['stargazerCount']
+    cursor = result['data']['search']['pageInfo']['endCursor']
+  # time.sleep(0.75)
 
 conn.commit()
 cur.close()
